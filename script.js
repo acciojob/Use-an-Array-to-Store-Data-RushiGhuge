076a192f135a42d15c2115412525550ce9404be7
Rushikesh Ghuge <100513228+RushiGhuge@users.noreply.github.com>
2023-08-09 06:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-
+let complexArray = [
+  [
+    {
+      one: "rushiikesh",
+      two: 2,
+		three:true
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      one: "rushiikesh",
+      two: 2,
+		three:true
+    },
+    {
+      one: "rushiikesh",
+      two: 2,
+		three:true
+    },
+	   {
+      one: "rushiikesh",
+      two: 2,
+		three:true
+    },
+	   {
+      one: "rushiikesh",
+      two: 2,
+		three:true
+    },
+	  
+  ]
